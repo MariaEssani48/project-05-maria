a9697b75c2a5087aa9af3a2d527e79bd7d6a17d1
Maria <mariaessani493@gmail.com>
2023-08-15 19:25:34 +0500

    Done

---
+++ dist/index.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from 'chalk-animation';
+import { createSpinner } from 'nanospinner';
+const sleep = () => {
+    return new Promise((r) => { setTimeout(r, 3000); });
+};
+async function spinAct() {
+    const spinner = createSpinner('OK! Counting your words...').start();
+    await sleep();
+    spinner.stop();
+}
+;
+async function welcome() {
+    let title = chalkAnimation.rainbow("WORD COUNTER"); //start heading
+    await sleep();
+    title.stop();
+}
+await welcome();
+//To count words
+async function countingWords(sentence) {
+    const myArr = sentence.split(" ");
+    let countValue = myArr.length;
+    return countValue;
+}
+let v = countingWords("How are you?");
+console.log(v);
+//To take user input and generate output
+async function userInput() {
+    const answer = await inquirer.
+        prompt([
+        /* Pass your questions in here */
+        {
+            type: "input",
+            name: "userWords",
+            message: "Type your sentence \n",
+        },
+    ]);
+    var result = await countingWords(answer.userWords);
+    await spinAct();
+    console.log(chalk.blue(`There are ${chalk.red(result)} words in your paragraph.`));
+}
+await userInput();
---
+++ readme.md
@@ -0,0 +1,4 @@
+TYPE "npx wordcount-maria"
+
+The small cli app will help you count the amount of words in your single paragraph.
+
